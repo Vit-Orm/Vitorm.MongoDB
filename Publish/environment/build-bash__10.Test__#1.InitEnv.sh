8e4e92279310b7d85e616a40b53db822f1957a5c
Lith <LithWang@outlook.com>
2024-12-30 19:26:51 +0800

    InitEnv

--- Publish/environment/build-bash__10.Test__#1.InitEnv.sh
+++ Publish/environment/build-bash__10.Test__#1.InitEnv.sh
@@ -15,26 +15,25 @@ export basePath=/root/temp
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #1 start MongoDB container'
 
 
-docker network create net-vitorm-mongodb || true
 docker rm vitorm-mongodb2 -f || true
 docker rm vitorm-mongodb -f || true
 
-docker run -d -p 27017:27017 --name vitorm-mongodb  --net net-vitorm-mongodb mongo:4.4.29 mongod --replSet my-mongo-set
-docker run -d -p 30001:27017 --name vitorm-mongodb2 --net net-vitorm-mongodb mongo:4.4.29 mongod --replSet my-mongo-set
+docker run -d --net host --name vitorm-mongodb  mongo:4.4.29 mongod --replSet my-mongo-set
+docker run -d --net host --name vitorm-mongodb2 mongo:4.4.29 mongod --port 27018 --replSet my-mongo-set
 
 
 #---------------------------------------------------------------------
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #2 wait for MongoDB to init'
-docker run -t --rm --net net-vitorm-mongodb mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;' | mongo --host vitorm-mongodb); do echo waiting for MongoDB; sleep 2; done;"
+docker run -t --rm --net host mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;' | mongo); do echo waiting for MongoDB; sleep 2; done;"
 
-docker run -t --rm --net net-vitorm-mongodb mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;' | mongo --host vitorm-mongodb2); do echo waiting for MongoDB; sleep 2; done;"
+docker run -t --rm --net host mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;' | mongo --host localhost --port 27018); do echo waiting for MongoDB; sleep 2; done;"
 
 
 #---------------------------------------------------------------------
 echo '#build-bash__10.Test__#1.InitEnv.sh -> #3 start replica set'
-docker run -t --rm --net net-vitorm-mongodb mongo:4.4.29 sh -c "echo 'rs.initiate({\"_id\":\"my-mongo-set\",\"members\":[{\"_id\":0,\"host\":\"vitorm-mongodb:27017\"},{\"_id\":1,\"host\":\"vitorm-mongodb2:27017\"}]})' | mongo --host vitorm-mongodb"
+docker run -t --rm --net host mongo:4.4.29 sh -c "echo 'rs.initiate({\"_id\":\"my-mongo-set\",\"members\":[{\"_id\":0,\"host\":\"localhost:27017\"},{\"_id\":1,\"host\":\"localhost:27018\"}]})' | mongo"
 
-docker run -t --rm --net net-vitorm-mongodb mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;\nuse db_orm2;' | mongo --host vitorm-mongodb); do echo waiting for MongoDB; sleep 2; done;"
+docker run -t --rm --net host mongo:4.4.29 timeout 120 sh -c "until (echo 'use db_orm;\nuse db_orm2;' | mongo); do echo waiting for MongoDB; sleep 2; done;"
 
 
 #---------------------------------------------------------------------
--- Publish/environment/build-bash__10.Test__#3.CleanEnv.sh
+++ Publish/environment/build-bash__10.Test__#3.CleanEnv.sh
@@ -18,6 +18,5 @@ echo '#build-bash__10.Test_#3.CleanEnv.sh'
 echo '#build-bash__10.Test_#3.CleanEnv.sh -> #1 remove MongoDB'
 docker rm vitorm-mongodb2 -f || true
 docker rm vitorm-mongodb -f || true
-docker network rm net-vitorm-mongodb || true
 
 
